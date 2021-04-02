b059a807381ca3d25791c32c96c068c1f0902479
AlexStepanchuk <666stepashka666@gmail.com>
2021-04-03 00:11:33 +0300

    feat: page with map is done

---
+++ online-zoo/pages/map/main.js
@@ -0,0 +1,5 @@
+//show search input in header
+const SEARCH = document.querySelector('.search-icon')
+SEARCH.addEventListener('click', (e) => {
+    e.target.previousElementSibling.focus();
+}, false);

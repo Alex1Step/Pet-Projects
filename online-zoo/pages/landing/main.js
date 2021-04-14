c23def50c9975915951cdfe3a0f6daac3b439ed9
Alex_Stepanchuk <666stepashka666@gmail.com>
2021-04-14 22:45:08 +0300

    fix: fix tooltip border

--- online-zoo/pages/landing/main.js
+++ online-zoo/pages/landing/main.js
@@ -1,5 +1,9 @@
-//show search input in header
-const SEARCH = document.querySelector('.search-icon')
-SEARCH.addEventListener('click', (e) => {
+//show search-input in header
+const SEARCH = document.querySelector(".search-icon");
+SEARCH.addEventListener(
+  "click",
+  (e) => {
     e.target.previousElementSibling.focus();
-}, false);
+  },
+  false
+);

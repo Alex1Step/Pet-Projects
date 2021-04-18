369610f0994a442c9d0d0f571914dd9838484250
Alex_Stepanchuk <666stepashka666@gmail.com>
2021-04-18 22:22:31 +0300

    feat: adapt 640 of all footer

--- online-zoo/pages/landing/main.js
+++ online-zoo/pages/landing/main.js
@@ -1,4 +1,4 @@
-//show search-input in header
+//show search input in header
 const SEARCH = document.querySelector(".search-icon");
 SEARCH.addEventListener(
   "click",
@@ -7,3 +7,10 @@ SEARCH.addEventListener(
   },
   false
 );
+
+//show burger menu
+const BURGER = document.querySelector(".burger");
+const BURGER_MENU = document.querySelector(".show-burger-menu");
+BURGER.addEventListener("click", (e) => {
+  BURGER_MENU.classList.toggle("show");
+});
